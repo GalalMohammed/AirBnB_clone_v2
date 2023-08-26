d20f587fe7ce2b9b722a1e67371b6123135af52e
dina <dinaelhadidy966@gmail.com>
2023-08-26 16:52:15 -0400

    fix typo and inherit base

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes
     Attributes:
         email(str): the user's email
@@ -13,7 +13,7 @@ class User(BaseModel):
         last_name(str): the user's last name
     """
     __tablename__ = "users"
-    email = Colomn(String(128), nullable=False)
-    password = Colomn(String(128), nullable=False)
-    first_name = Colomn(String(128))
-    last_name = Colomn(String(128))
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
