01cb3cf04a4ef7c857e8a85beed21d815e0ea06b
GalalMohammed <galalmoh2001@gmail.com>
2023-08-27 21:38:48 +0300

    update Place, User, City classes

--- models/user.py
+++ models/user.py
@@ -2,18 +2,21 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes
+    """This class defines a user by various attributes.
     Attributes:
-        email(str): the user's email
-        password(str): the user's password
-        first_name(str): the user's first name
-        last_name(str): the user's last name
+        email(str): the user's email.
+        password(str): the user's password.
+        first_name(str): the user's first name.
+        last_name(str): the user's last name.
+        places (object): user linked places.
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade='delete')
