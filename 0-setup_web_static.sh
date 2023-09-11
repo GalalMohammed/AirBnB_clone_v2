7289bbb4951467ed4fe9e02cea323ded8c54b81f
GalalMohammed <galalmoh2001@gmail.com>
2023-09-12 01:08:42 +0300

    fix sed args

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,10 +7,7 @@ touch /data/web_static/releases/test/index.html
 echo 'Holberton School' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-loc="
-location \/hbnb_static {
-	alias \/data\/web_static\/current\/;
-}"
+loc="\nlocation \/hbnb_static {\nalias \/data\/web_static\/current\/;\n}"
 line="server_name _;"
 sed -i "s/$line/$line\n$loc/" /etc/nginx/sites-available/default
 service nginx restart
