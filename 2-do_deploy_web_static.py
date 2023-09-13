4b21b66439280372244674b1ebc9fbe5a280678a
GalalMohammed <galalmoh2001@gmail.com>
2023-09-14 01:58:28 +0300

    work locally

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 
 
 from pathlib import Path
-from fabric.api import put, env, run
+from fabric.api import put, env, run, local
 
 env.hosts = ["54.208.56.50", "54.90.5.188"]
 
@@ -28,23 +28,31 @@ def do_deploy(archive_path: str) -> bool:
 
     # Upload the archive to the /tmp/ directory of the web server
     result = put(archive_path, "/tmp/" + archive_filename)
+    result = local("mv " + archive_path + " /tmp")
 
     if result.succeeded:
         # Create the target directory for the release
         result = run("mkdir -p /data/web_static/releases/" + archive_basename)
+        result = local("mkdir -p /data/web_static/releases/" + archive_basename)
 
     if result.succeeded:
         # Uncopmress the archive into the target directory
         result = run("tar -xzf /tmp/" + archive_filename +
                      " -C /data/web_static/releases/" + archive_basename)
+        result = local("tar -xzf /tmp/" + archive_filename +
+                     " -C /data/web_static/releases/" + archive_basename)
 
     if result.succeeded:
         # Remove the uploaded archive
         result = run("rm /tmp/" + archive_filename)
+        result = local("rm /tmp/" + archive_filename)
 
     if result.succeeded:
         # Move the contents to the current directory
         result = run("mv /data/web_static/releases/" + archive_basename +
+                     "/web_static/* /data/web_static/releases/" +
+                     archive_basename)
+        result = local("mv /data/web_static/releases/" + archive_basename +
                      "/web_static/* /data/web_static/releases/" +
                      archive_basename)
 
@@ -52,10 +60,15 @@ def do_deploy(archive_path: str) -> bool:
         # Remove the empty web_static directory
         result = run("rm -rf /data/web_static/releases/" + archive_basename +
                      "/web_static")
+        result = local("rm -rf /data/web_static/releases/" + archive_basename +
+                     "/web_static")
 
     if result.succeeded:
         # Update the symbolic link tests
         run("rm -rf /data/web_static/current")
         result = run("ln -s /data/web_static/releases/" + archive_basename +
+                     " /data/web_static/current")
+        local("rm -rf /data/web_static/current")
+        result = local("ln -s /data/web_static/releases/" + archive_basename +
                      " /data/web_static/current")
     return result.succeeded
